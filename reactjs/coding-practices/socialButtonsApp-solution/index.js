495fd6ac0bdbd3778d03ee298251a9519e711a53
codedbylawan <lawan.goud.dev@gmail.com>
2026-04-25 07:19:08 +0000

    SocialButtonsApp

--- reactjs/coding-practices/socialButtonsApp-solution/index.js
+++ reactjs/coding-practices/socialButtonsApp-solution/index.js
@@ -1,11 +1,10 @@
-const Button = (props) => {
-  //  Write your code here.
-  const { className, buttonText } = props;
-  return <button className={`button ${className}`}>{buttonText}</button>;
-};
+const Button = props => {
+  const {className, buttonText} = props
+  
+  return <button className={`button ${className}`}>{buttonText}</button>
+}
 
 const element = (
-  //  Write your code here.
   <div className="social-buttons-container">
     <div className="content-container">
       <h1 className="heading">Social Buttons</h1>
@@ -16,6 +15,6 @@ const element = (
       </div>
     </div>
   </div>
-);
+)
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById('root'))
